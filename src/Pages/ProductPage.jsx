f920b73e7155590327ad93654611bf6fbd53255b
Octavio Noval <163550043+OctavioDNoval@users.noreply.github.com>
2025-10-20 18:49:18 -0300

    Add product fetching logic to ProductPage
    Introduced state and useEffect hooks to fetch products from the backend API on mount. Added loading and error state management, and included a section title for the product page.

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import { data } from "react-router-dom";
+
 export const ProductPage = () => {
-	return <section className="product-page"></section>;
+	const [products, setProducts] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState();
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			setIsLoading(true);
+			try {
+				const res = await fetch("http://localhost:8080/products/public/getAll");
+				const data = await res.json();
+				setProducts(data);
+				console.log(data);
+			} catch (err) {
+				console.error(err);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		fetchProducts();
+	}, []);
+
+	useEffect(() => {
+		console.log(products);
+	}, [products]);
+
+	return (
+		<section className="product-page">
+			<h2 className="product-page-title">Productos</h2>
+		</section>
+	);
 };
